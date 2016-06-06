3f685427f68f96e15ce4e240c29409810b81f6e7
Duda Dornelles <dudassdornelles@gmail.com>
2016-06-06 18:42:49 -0500

    Adding ability to not verify ssl certificate
    This showed useful when testing in my local machine with SSL but an
    invalid certificate

--- gomatic/go_cd_configurator.py
+++ gomatic/go_cd_configurator.py
@@ -205,7 +205,7 @@ class GoCdConfigurator(object):
 
 
 class HostRestClient(object):
-    def __init__(self, host, username=None, password=None, ssl=False):
+    def __init__(self, host, username=None, password=None, ssl=False, verify_ssl=True):
         self.__host = host
         self.__username = username
         self.__password = password
@@ -222,11 +222,11 @@ class HostRestClient(object):
         return (self.__username, self.__password) if self.__username or self.__password else None
 
     def get(self, path):
-        return requests.get(self.__path(path), auth=self.__auth())
+        return requests.get(self.__path(path), auth=self.__auth(), verify=verify_ssl)
 
     def post(self, path, data):
         url = self.__path(path)
-        result = requests.post(url, data, auth=self.__auth())
+        result = requests.post(url, data, auth=self.__auth(), verify=verify_ssl)
         if result.status_code != 200:
             try:
                 result_json = json.loads(result.text.replace("\\'", "'"))
